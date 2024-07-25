2da52c5cadc4f8c2f1aa58ffdfd12401b5a15816
LewisZZZ-Zhang <157969244+LewisZZZ-Zhang@users.noreply.github.com>
2024-07-25 14:58:09 +0800

    update

--- OOP1/sketch.js
+++ OOP1/sketch.js
@@ -1,11 +1,21 @@
+let a 
+
 function setup() {
     let cnv = createCanvas(400, 400);
     cnv.parent('sketch-holder');
-  }
-  
+    a = new taxi();
+    console.log(a);
+}
+
 function draw() {
     background(0);
-    fill(255, 255, 255);
-    circle(200, 200+200*sin(frameCount/40+PI/2), 100);
-    circle(200-200*sin(frameCount/40), 200, 100);
-  }
+    fill(255,0,0);
+}
+
+class taxi{
+    constructor(){
+        this.x = 100;
+        this.y = 100;
+        this.s = 0.5
+    }
+}
