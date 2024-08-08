20063b6bb78a146a623387e1638cfb0d09141528
LewisZZZ-Zhang <157969244+LewisZZZ-Zhang@users.noreply.github.com>
2024-08-08 13:23:58 +0800

    1

--- Build_a_Gundam/Build_a_Gundam_leg1/sketch.js
+++ Build_a_Gundam/Build_a_Gundam_leg1/sketch.js
@@ -202,9 +202,9 @@ function preload() {
     part3Img = loadImage('assets/head3.png', () => console.log('part3Img loaded'), () => console.error('Failed to load part3Img'));
     part4Img = loadImage('assets/head4.png', () => console.log('part4Img loaded'), () => console.error('Failed to load part4Img'));
     guideImg = loadImage('assets/guide.png', () => console.log('guidebook loaded'), () => console.error('Failed to load guidebook'));
-    wrongsign = loadImage('assets/wrong.png', () => console.log('wrongsign loaded'), () => console.error('Failed to load wrongsign'));
+    wrongsign = loadImage('assets/Wrong.png', () => console.log('wrongsign loaded'), () => console.error('Failed to load wrongsign'));//
     for (let i = 0; i < 4; i++) {
-        sprues[i] = loadImage(`assets/sprue${i + 1}.png`);
+        sprues[i] = loadImage(`assets/Sprue${i + 1}.png`);//
     }
     cut1 = loadSound('assets/cut1.mp3', () => console.log('cut1 loaded'), () => console.error('Failed to load cut1'));
     cut2 = loadSound('assets/cut2.mp3', () => console.log('cut2 loaded'), () => console.error('Failed to load cut2'));
